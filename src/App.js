b5293c7fc9103eda3c16bedd9413b3613cdc0852
Lu Mike <myatkaungg31@gmail.com>
2022-10-03 04:35:31 +0000

    move blog list code to blogList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogList from './components/BlogList'
 import blogService from './services/blogs'
 
 const App = () => {
@@ -12,12 +13,7 @@ const App = () => {
   }, [])
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    <BlogList blogs = {blogs}/>
   )
 }
 
