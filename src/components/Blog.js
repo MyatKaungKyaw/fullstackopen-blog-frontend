5bc04a6659ea83665ed22653e7710e6f0e9f442d
Lu Mike <myatkaungg31@gmail.com>
2022-10-11 13:29:18 +0000

    not finish. show/hide blog detail

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import blogs from "../services/blogs"
+import Togglable from "./Togglable"
 
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const likeClick = (e) => {
+    console.log('likeclick')
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title} {blog.author}</p>
+      <Togglable >
+        <>
+          <div>{blog.url}</div>
+          <div>
+            <p>likes : {blog.likes}</p> 
+            <button onClick={likeClick}>like</button>
+            </div>
+          <div>{blogs.user.name}</div>
+        </>
+      </Togglable>
+    </div>
+  )
+}
 export default Blog
