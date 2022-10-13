0cdaa1bc984ac04d4efbf74af3051d4632b661ab
Lu Mike <myatkaungg31@gmail.com>
2022-10-13 04:30:58 +0000

    modify blog border and update show hide button

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,16 @@
-import Togglable from "./Togglable"
+import { useState } from 'react'
 
 const Blog = ({ blog, handleLikeClick, user, deleteBlog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const show = (e) => {
+    setVisible(true)
+  }
+
+  const hide = (e) => {
+    setVisible(false)
+  }
+
   const likeClick = async (e) => {
     await handleLikeClick(blog)
   }
@@ -13,20 +23,20 @@ const Blog = ({ blog, handleLikeClick, user, deleteBlog }) => {
 
   return (
     <div className='blog-style'>
-      <p>{blog.title} {blog.author}</p>
-      <Togglable text='view' cancelText='hide'>
-        <>
-          <a href={blog.url} target='_blank' rel="noreferrer" className='blog-url'>{blog.url}</a>
-          <div>
-            <p className='blog-like'>likes : {blog.likes} </p>
-            <button onClick={likeClick} className='blog-like'>like</button>
-          </div>
-          {blog.user && <div>{blog.user.name}</div>}
-          {blog.user && blog.user.username === user.username &&
-            <button onClick={deleteClick} className="delete-button">remove</button>
-          }
-        </>
-      </Togglable>
+      <p className='inline'>{blog.title} {blog.author}</p>
+      {!visible && <button onClick={show} className='inline'>view</button>}
+      {visible && <>
+        <button onClick={hide} className='inline'>hide</button>
+        <a href={blog.url} target='_blank' rel="noreferrer" className='blog-url'>{blog.url}</a>
+        <div>
+          <p className='blog-like'>likes : {blog.likes} </p>
+          <button onClick={likeClick} className='blog-like'>like</button>
+        </div>
+        {blog.user && <div>{blog.user.name}</div>}
+        {blog.user && blog.user.username === user.username &&
+          <button onClick={deleteClick} className="delete-button">remove</button>
+        }
+      </>}
     </div>
   )
 }
