bc11e2164b90d9f56ee37669724ebfb813688db1
Lu Mike <myatkaungg31@gmail.com>
2022-10-25 10:29:06 +0000

    modify blog test file

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -3,16 +3,30 @@ import '@testing-library/jest-dom'
 import {render,screen} from '@testing-library/react'
 import Blog from './Blog'
 
-test('Blog component renders only title and author as default',() => {
-    const blog = {
-        title: 'black adam review',
-        author: 'rotten tomato',
-        url: 'https://www.rottentomatoes.com/m/black_adam',
-        likes: 39
-    }
+describe('Blog component', () => {
+    let container
 
-    render(<Blog blog={blog}/>)
+    beforeEach(() => {
+        const blog = {
+            title: 'black adam review',
+            author: 'rotten tomato',
+            url: 'https://www.rottentomatoes.com/m/black_adam',
+            likes: 39
+        }
+    
+        container = render(<Blog blog={blog}/>).container
+    })
+    
+    test('renders only title and author as default',() => {   
+        const div = screen.getByText('black adam review rotten tomato')
+        expect(div).toBeDefined()
+    })
+    
+    test('at start url and likes are not rendered',() => {
+        const a = container.querySelector('.blog-link')
+        // const a = screen.getByText('https://www.rottentomatoes.com/m/black_adam')
 
-    const div = screen.getByText('black adam review rotten tomato')
-    expect(div).toBeDefined()
+        screen.debug(a)
+        expect(a).toBeDefined()
+    })
 })
