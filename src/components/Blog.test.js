ec2edec05540c37b5464f92e10fcd85a901a8df5
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-10-26 21:35:36 +0630

    add like button test

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import {render,screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog component', () => {
     let container
+    let handleLikeClick
 
     beforeEach(() => {
         const blog = {
@@ -14,21 +15,35 @@ describe('Blog component', () => {
             url: 'https://www.rottentomatoes.com/m/black_adam',
             likes: 39
         }
-    
-        container = render(<Blog blog={blog}/>).container
+        const user = {}
+        //mock function
+        handleLikeClick = jest.fn()
+        const deleteBlog = jest.fn()
+
+        container = render(
+            <Blog
+                blog={blog}
+                handleLikeClick={handleLikeClick}
+                user={user}
+                deleteBlog={deleteBlog}
+            />
+        ).container
     })
-    
-    test('renders only title and author as default',() => {   
+
+    test('renders only title and author as default', () => {
         const div = screen.getByText('black adam review rotten tomato')
         expect(div).toBeDefined()
     })
-    
-    test('at start url and likes are not rendered',() => {
+
+    test('at start url and likes are not rendered', () => {
         const a = screen.queryByText('https://www.rottentomatoes.com/m/black_adam')
         expect(a).not.toBeInTheDocument()
+
+        const likes = screen.queryByText('likes : 39')
+        expect(likes).not.toBeInTheDocument()
     })
 
-    test('after click to view button, details are display',async () => {
+    test('after click to view button, details are display', async () => {
         //apply workarounds and mock the UI layer to simulate user 
         //interactions like they would happen in the browser.
         const user = userEvent.setup()
@@ -42,5 +57,18 @@ describe('Blog component', () => {
         const likes = screen.getByText('likes : 39')
         expect(likes).toBeDefined()
     })
-    
+
+    test('calls like onClick function', async () => {
+        //apply workarounds and mock the UI layer to simulate user 
+        //interactions like they would happen in the browser.
+        const user = userEvent.setup()
+
+        const viewButton = screen.getByText('view')
+        await user.click(viewButton)
+
+        const likeButton = screen.getByText('like')
+        await user.dblClick(likeButton)
+
+        expect(handleLikeClick.mock.calls).toHaveLength(2)
+    })
 })
