f0225ae85d1c37188d26be5e42eca30c345f1e11
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-10-26 21:35:54 +0630

    create createblog test file

---
+++ src/components/CreateBlog.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import {render,screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import CreateBlog from './CreateBlog'
+
+describe('Create blog form',() => {
+    beforeEach(() => {
+        // render()
+    })
+})
